0a4a5d8bc58d9e92ab1dc4385500f26b2f535271
AmeerSafadi-95 <ameersafadi95@gmail.com>
2020-08-02 20:13:40 +0300

    Updates

---
+++ precache-manifest.5836ca8fd3093f2ab5316e4143933afc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a53a7c4394ff6c1582507011524f2753",
+    "url": "/AmeerShop/index.html"
+  },
+  {
+    "revision": "fc441c10b07eb9694569",
+    "url": "/AmeerShop/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "00cd639d5df49335d90f",
+    "url": "/AmeerShop/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fc441c10b07eb9694569",
+    "url": "/AmeerShop/static/js/2.f4e901cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AmeerShop/static/js/2.f4e901cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00cd639d5df49335d90f",
+    "url": "/AmeerShop/static/js/main.3d71b60c.chunk.js"
+  },
+  {
+    "revision": "d569e2d6789235f96bdf",
+    "url": "/AmeerShop/static/js/runtime-main.2ee2b7fb.js"
+  },
+  {
+    "revision": "b7947845da46ea22fbd4709828ece648",
+    "url": "/AmeerShop/static/media/a1.b7947845.jpg"
+  },
+  {
+    "revision": "37dc673dc964af04c9f8f3f7db77753f",
+    "url": "/AmeerShop/static/media/a2.37dc673d.png"
+  },
+  {
+    "revision": "8f06b4814ca31558c8bf49e9d393c406",
+    "url": "/AmeerShop/static/media/a3.8f06b481.jpg"
+  },
+  {
+    "revision": "138fce3c1d1b9b3de2dca8339e955ba5",
+    "url": "/AmeerShop/static/media/a4.138fce3c.jpg"
+  }
+]);
